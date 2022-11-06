34539dc0fcac5fc3f50eb6ce75fbf7c33f4ba959
fawii1212 <fawad.fa3@gmal.com>
2022-11-06 23:56:21 +0500

    first commit

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,25 +1,15 @@
 import { createWebHistory, createRouter } from "vue-router";
 import home from './Components/home.vue';
-import login from './Components/login.vue';
-import register from './Components/register.vue';
+import login from './Components/auth/login.vue';
+import register from './Components/auth/register.vue';
+import forget from './Components/auth/forget.vue';
 
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: home,
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: login,
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: register,
-    }
+    { path: '/', name: 'Home', component: home, },
+    { path: '/login', name: 'Login', component: login, },
+    { path: '/register', name: 'Register', component: register, },
+    { path: '/forget', name: 'forget', component: forget, },
 ];
 
 const router = createRouter({
